8d3521784c99841a5e3d99933d58c19b16c44acd
Wallace Vieira <wallaceofc@hotmail.com>
2022-11-02 21:46:14 -0300

    Comittando meu projeto de React

--- src/App.js
+++ src/App.js
@@ -1,25 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import './styles/App.css';
+import userIcon from './images/user.svg';
+import paperPlaneIcon from './images/paper-plane.svg';
+import './styles/PostForm.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default function App(){
+    return (
+        <div className='wrapper'>
+            <form className='post-form' onSubmit={()=>alert('formulário submetido')}>
+                <input placeholder='Escreva uma nova história...' />
+                
+                <div>
+                    <img src={userIcon} alt='user'/>
+
+                    <input placeholder='Digite seu nome...' />
+
+                    <button type='submit'>
+                        <img src={paperPlaneIcon} alt='Paper plane' />
+
+                        Publicar
+                    </button>
+                </div>
+
+            </form>
+        </div>
+    );
 }
-
-export default App;
