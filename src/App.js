000cada07ba8afba8de353d5b16d0ec57da275b7
Wallace Vieira <wallaceofc@hotmail.com>
2022-11-13 11:20:12 -0300

    Adicionando event submit, e valor digitado nos posts

--- src/App.js
+++ src/App.js
@@ -1,79 +1,105 @@
-import './styles/App.css';
-import './styles/PostForm.css';
-import './styles/Feed.css';
+import "./styles/App.css";
+import "./styles/PostForm.css";
+import "./styles/Feed.css";
 
-import userIcon from './images/user.svg';
-import paperPlaneIcon from './images/paper-plane.svg';
-import clockIcon from './images/clock.svg';
-import emptyFolderIcon from './images/empty-folder.svg';
+import userIcon from "./images/user.svg";
+import paperPlaneIcon from "./images/paper-plane.svg";
+import clockIcon from "./images/clock.svg";
+import emptyFolderIcon from "./images/empty-folder.svg";
+import { useState } from "react";
 
 export default function App() {
+  const [history, setHistory] = useState("");
+  const [userName, setUserName] = useState("");
+  const [posts, setPosts] = useState([]);
 
-    const posts = [
-        {
-            id: Math.random(),
-            content: 'Conteúdo do post',
-            userName: 'Mateus',
-            publishedAt: new Date(),
-        },
-    ];
+  function handleSubmit(event) {
+    event.preventDefault();
 
-    return (
-        <div className="wrapper">
-            <form className="post-form" onSubmit={() => alert('formulário submetido')}>
-                <input placeholder="Escreva uma nova história..." />
-                <div>
-                    <img src={userIcon} alt="user" />
-                    <input placeholder='Digite seu nome...' />
-                    <button type='submit'>
-                        <img src={paperPlaneIcon} alt='Paper plane' />
-                        Publicar
-                    </button>
-                </div>
-            </form>
-            
-            <main>
-                {posts.length === 0 ? (
-                    <div className="feed-status">
-                        <img src={emptyFolderIcon} alt="Empty Folder" />
-                        <h1>Não encontramos nada</h1>
-                        <h2>
-                            Parece que você e seus amigos não postaram nada.
-                            Comece a escrever uma nova história!
-                        </h2>
-                    </div>
-                ) : null}
-            
-                {posts.length > 0 ? (
-                    <>
-                        <header>
-                            <h1>Seu Feed</h1>
-                            <h2>Acompanhe o que seus amigos estão pensando em tempo real</h2>
-                        </header>
-                        
-                        <section className="feed">
-                            {posts.map((post) => (
-                                <article key={post.id}>
-                                    <p>
-                                        {post.content}
-                                    </p>
-                                    <footer>
-                                        <div className="user-details">
-                                            <img src={userIcon} alt="User" />
-                                            <strong>{post.userName}</strong>
-                                        </div>
-                                        
-                                        <div className="time">
-                                            <img src={clockIcon} alt="Clock" />
-                                            <span>Publicado em {post.publishedAt.toLocaleDateString('pt-br')}</span>
-                                        </div>
-                                    </footer>
-                                </article>
-                            ))}
-                        </section>
-                    </>
-                ) : null}
-            </main>
+    setPosts([
+      ...posts,
+      {
+        id: Math.random(),
+        content: history,
+        userName,
+        publishedAt: new Date(),
+      },
+    ]);
+
+    setHistory('')
+    setUserName('')
+  }
+
+  return (
+    <div className="wrapper">
+      <form className="post-form" onSubmit={handleSubmit}>
+        <input
+          value={history}
+          placeholder="Escreva uma nova história..."
+          onChange={(event) => setHistory(event.target.value)}
+        />
+        <div>
+          <img src={userIcon} alt="user"
+          />
+          <input
+          value={userName}
+            placeholder="Digite seu nome..."
+            onChange={(event) => setUserName(event.target.value)}
+          />
+          <button type="submit">
+            <img src={paperPlaneIcon} alt="Paper plane"
+            />
+            Publicar
+          </button>
         </div>
-    );
+      </form>
+
+      <main>
+        {posts.length === 0 ? (
+          <div className="feed-status">
+            <img src={emptyFolderIcon} alt="Empty Folder"
+            />
+            <h1>Não encontramos nada</h1>
+            <h2>
+              Parece que você e seus amigos não postaram nada. Comece a escrever
+              uma nova história!
+            </h2>
+          </div>
+        ) : null}
+
+        {posts.length > 0 ? (
+          <>
+            <header>
+              <h1>Seu Feed</h1>
+              <h2>Acompanhe o que seus amigos estão pensando em tempo real</h2>
+            </header>
+
+            <section className="feed">
+              {posts.map((post) => (
+                <article key={post.id}>
+                  <p>{post.content}</p>
+                  <footer>
+                    <div className="user-details">
+                      <img src={userIcon} alt="User"
+                      />
+                      <strong>{post.userName}</strong>
+                    </div>
+
+                    <div className="time">
+                      <img src={clockIcon} alt="Clock"
+                      />
+                      <span>
+                        Publicado em{" "}
+                        {post.publishedAt.toLocaleDateString("pt-br")}
+                      </span>
+                    </div>
+                  </footer>
+                </article>
+              ))}
+            </section>
+          </>
+        ) : null}
+      </main>
+    </div>
+  );
 }
