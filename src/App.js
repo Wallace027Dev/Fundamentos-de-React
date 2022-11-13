d3684bea46801a205990b520d8fbeeabc5d531f4
Wallace Vieira <wallaceofc@hotmail.com>
2022-11-13 12:20:39 -0300

    Refatorando Feed

--- src/App.js
+++ src/App.js
@@ -2,11 +2,11 @@ import "./styles/App.css";
 import "./styles/PostForm.css";
 import "./styles/Feed.css";
 
-import userIcon from "./images/user.svg";
 import paperPlaneIcon from "./images/paper-plane.svg";
-import clockIcon from "./images/clock.svg";
-import emptyFolderIcon from "./images/empty-folder.svg";
 import { useState } from "react";
+import Feed from "../src/components/Feed";
+import userIcon from './images/user.svg';
+
 
 export default function App() {
   const [history, setHistory] = useState("");
@@ -55,50 +55,7 @@ export default function App() {
       </form>
 
       <main>
-        {posts.length === 0 ? (
-          <div className="feed-status">
-            <img src={emptyFolderIcon} alt="Empty Folder"
-            />
-            <h1>Não encontramos nada</h1>
-            <h2>
-              Parece que você e seus amigos não postaram nada. Comece a escrever
-              uma nova história!
-            </h2>
-          </div>
-        ) : null}
-
-        {posts.length > 0 ? (
-          <>
-            <header>
-              <h1>Seu Feed</h1>
-              <h2>Acompanhe o que seus amigos estão pensando em tempo real</h2>
-            </header>
-
-            <section className="feed">
-              {posts.map((post) => (
-                <article key={post.id}>
-                  <p>{post.content}</p>
-                  <footer>
-                    <div className="user-details">
-                      <img src={userIcon} alt="User"
-                      />
-                      <strong>{post.userName}</strong>
-                    </div>
-
-                    <div className="time">
-                      <img src={clockIcon} alt="Clock"
-                      />
-                      <span>
-                        Publicado em{" "}
-                        {post.publishedAt.toLocaleDateString("pt-br")}
-                      </span>
-                    </div>
-                  </footer>
-                </article>
-              ))}
-            </section>
-          </>
-        ) : null}
+        <Feed posts={posts}/>
       </main>
     </div>
   );
