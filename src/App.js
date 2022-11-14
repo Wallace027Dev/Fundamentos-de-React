b8a6e7a0a5a7743cd2b67226aa8cbc25b95047d5
Wallace Vieira <wallaceofc@hotmail.com>
2022-11-14 16:47:13 -0300

    Adicionando aba de "most viewed", finalizando curso

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import "./styles/App.css";
 import AppRoutes from "./Routes";
-import {BrowserRouter} from 'react-router-dom';
+import {BrowserRouter, Link} from 'react-router-dom';
 
 export default function App() {
 
   return (
     <div className="wrapper">
       <BrowserRouter>
+        <nav>
+          <Link to="/">Meu feed</Link>
+          <Link to="/most-viewed">Mais vistos</Link>
+        </nav>
         <AppRoutes />
       </BrowserRouter>
     </div>
