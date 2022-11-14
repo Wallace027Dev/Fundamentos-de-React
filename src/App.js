675a18cd491770c3ccc956ee0b7a6d0207191fc6
Wallace Vieira <wallaceofc@hotmail.com>
2022-11-14 16:13:54 -0300

    Adicionando routes.js

--- src/App.js
+++ src/App.js
@@ -1,62 +1,14 @@
 import "./styles/App.css";
-import "./styles/PostForm.css";
-import "./styles/Feed.css";
-
-import paperPlaneIcon from "./images/paper-plane.svg";
-import { useState } from "react";
-import Feed from "../src/components/Feed";
-import userIcon from './images/user.svg';
-
+import AppRoutes from "./Routes";
+import {BrowserRouter} from 'react-router-dom';
 
 export default function App() {
-  const [history, setHistory] = useState("");
-  const [userName, setUserName] = useState("");
-  const [posts, setPosts] = useState([]);
-
-  function handleSubmit(event) {
-    event.preventDefault();
-
-    setPosts([
-      ...posts,
-      {
-        id: Math.random(),
-        content: history,
-        userName,
-        publishedAt: new Date(),
-      },
-    ]);
-
-    setHistory('')
-    setUserName('')
-  }
 
   return (
     <div className="wrapper">
-      <form className="post-form" onSubmit={handleSubmit}>
-        <input
-          value={history}
-          placeholder="Escreva uma nova história..."
-          onChange={(event) => setHistory(event.target.value)}
-        />
-        <div>
-          <img src={userIcon} alt="user"
-          />
-          <input
-          value={userName}
-            placeholder="Digite seu nome..."
-            onChange={(event) => setUserName(event.target.value)}
-          />
-          <button type="submit">
-            <img src={paperPlaneIcon} alt="Paper plane"
-            />
-            Publicar
-          </button>
-        </div>
-      </form>
-
-      <main>
-        <Feed posts={posts}/>
-      </main>
+      <BrowserRouter>
+        <AppRoutes />
+      </BrowserRouter>
     </div>
   );
 }
---
+++ src/components/PostForm.js
@@ -0,0 +1,42 @@
+import "../styles/PostForm.css";
+import userIcon from '../images/user.svg';
+import paperPlaneIcon from "../images/paper-plane.svg";
+import { useState } from "react";
+
+export default function PostForm(props){
+  const [history, setHistory] = useState("");
+  const [userName, setUserName] = useState("");
+
+  function handleSubmit(event) {
+    event.preventDefault();
+
+      props.onSubmit({history, userName});
+    
+    setHistory('')
+    setUserName('')
+  }
+
+  return (
+    <form className="post-form" onSubmit={handleSubmit}>
+    <input
+      value={history}
+      placeholder="Escreva uma nova história..."
+      onChange={(event) => setHistory(event.target.value)}
+    />
+    <div>
+      <img src={userIcon} alt="user"
+      />
+      <input
+      value={userName}
+        placeholder="Digite seu nome..."
+        onChange={(event) => setUserName(event.target.value)}
+      />
+      <button type="submit">
+        <img src={paperPlaneIcon} alt="Paper plane"
+        />
+        Publicar
+      </button>
+    </div>
+  </form>
+  )
+}
